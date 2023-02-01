116334194d2119ba7b74b8144036d18ff2c9180a
Henry Joerg <henry.joerg@gmail.com>
2023-02-01 09:05:28 +0100

    add datepicker

--- lib/screens/edit_journal_entry.dart
+++ lib/screens/edit_journal_entry.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore_platform_interface/src/timestamp.dart';
 import 'package:flutter/material.dart';
 import 'package:lernjournal/models/journal_entry.dart';
 
@@ -12,11 +13,13 @@ class EditJournalEntry extends StatefulWidget {
 
 class _EditJournalEntryState extends State<EditJournalEntry> {
   final TextEditingController _textController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
 
   @override
   void initState() {
     super.initState();
     _textController.text = widget.data.text;
+    _selectedDate = widget.data.dateAsDateTime;
   }
 
   @override
@@ -24,14 +27,33 @@ class _EditJournalEntryState extends State<EditJournalEntry> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          widget.data.localDate,
-        ),
+            '${_selectedDate.day}.${_selectedDate.month}.${_selectedDate.year}'),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            Row(
+              children: [
+                Text(
+                    '${_selectedDate.day}.${_selectedDate.month}.${_selectedDate.year}'),
+                ElevatedButton(
+                  child: const Text('Select date'),
+                  onPressed: () async {
+                    DateTime? newDate = await showDatePicker(
+                      context: context,
+                      initialDate: _selectedDate,
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime.now(),
+                    );
+                    if (newDate == null) return;
+
+                    setState(() => _selectedDate = newDate);
+                  },
+                ),
+              ],
+            ),
             TextField(
               controller: _textController,
               decoration: const InputDecoration(
